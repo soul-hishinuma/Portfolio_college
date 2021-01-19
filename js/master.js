0e0cc84e0f1425cec4736fd514505acdd7eb9bdc
soul-hishinuma <soul00@outlook.jp>
2021-01-20 03:32:18 +0900

    first commit

---
+++ js/master.js
@@ -0,0 +1,52 @@
+const place = [
+  {
+    "name": "About Me",
+    "Overview": "サムネイルは名刺デザインです。使用ツールはIllustratorです。このページでは私について説明を行います。このポートフォリオに載せていないデザイン作品は是非vivivit(https://www.vivivit.com/profile/public/zJGLvA12MQ)をごらんください！",
+		"History": "作品を知っていただく前に私がどういった人間なのかを知っていただくことで相互的な相性や求めている人材に適しているかを判断していただけたら幸いです。",
+		"Detail": "こんにちは！宮城大学事業構想学群価値創造デザイン学類の菱沼奏流（ヒシヌマソウル）です。2000年2月21日生まれの20歳です。私は「理由追求マン」です。私は自分が理解できていないと行動できない人間です。 例を上げましょう。 私は学生時代Webについて・デザインについての勉強に力を入れました。 その中でこれは実際に重要なことですが、このサイト・アプリケーションはどんな意図でこのUIを使用しているのか、どんなUXを想定してつくられているのかが説明できない作品はぱっと見優れていても評価をつけることができませんでした。 反対に、その説明が自分なりにできている作品は改善の余地があったと感じるし、良くも悪くも明確な評価をつけることができました。 もう一つ例を上げましょう。 私は趣味で筋トレを行っています。これにも自分なりの理由があります。 １つは体型維持。見た目は人の印象を大きく左右すると考えています。 ２つ目は健康維持。健康でないと100%のパフォーマンスを発揮できません。 ３つ目は自己肯定感の向上。これはやってみて初めて気づいたのですが、筋トレを行うことでする前より格段にメンタルヘルスが向上しました。 このように力を入れたこと・趣味１つとっても私が自身の行動に理由を求める人間だということが分かります。 これは論理的に考えることができるという私の強みです。 ですが、筋トレの３つ目の理由のようにやってみないと分からないことというのも多々あります。行動の前に考えすぎてしまうというのが私の弱みです。 良くも悪くもこれが私です。現在はこの弱みをできるだけなくせるよう様々なことにチャレンジしています。",
+  },
+  {
+    "name": "Malaysia",
+    "Overview": "The name 'Malaysia' is a combination of the word 'Malay' and the Latin-Greek suffix '-sia'",
+		"History": "Evidence of modern human habitation in Malaysia dates back 40,000 years.",
+		"Detail": "Malaysia is the 66th largest country by total land area, with a land area of 329,613 km2 (127,264 sq mi).",
+  },
+  {
+    "name": "宮城大学",
+    "Overview": "宮城大学の新デザイン研究棟のPRを目的としたWebサイトのデザインです。作成期間は1週間で1人で作成しました。使用ツールはFigma、Photoshopです。Figma URL：https://www.figma.com/file/1lgnlGbvYgwDlJK63u1QMS/myu-design_2?node-id=0%3A1",
+		"History": "宮城大学の新デザイン研究棟が完成したため、そのPRを兼ねたWebサイトの設計を大学側からの依頼という設定で講義の課題として課されました。初めて1から情報設計からデザインまでを手がけた作品です。先進性・シンプルさを重視しました。",
+		"Detail": "The land area of the entire United States is approximately 3,800,000 square miles (9,841,955 km2),",
+  },
+  {
+    "name": "ヘルシェア！",
+    "Overview": "毎日使いたくなるヘルスケアアプリのUIデザイン。作成期間は2週間で1人で作成しました。使用ツールはFigma、Photoshopです。Figma URL：https://www.figma.com/file/sEhKOxfUmpmqRHSbeIqK3z/HealthShare?node-id=0%3A1",
+		"History": "現在流通しているヘルスケアアプリケーションはカラフルなデザインが多く、自身のゴールを定めることで健康を増進していくというクローズドなアプリケーションが多いので様々な人が使える配色、フォント、レイアウトかつSNS的に生活を共有するという機能をフィーチャーしたアプリケーションをデザインすることでヘルスケアアプリケーションで一番重要なモチベーション管理を別の視点から実現したいと考えた。",
+		"Detail": "色覚に障がいがある方を考慮してむやみに色の種類を多くつかわないことを特に注意しました。また、フォントはできるだけ大きく可読性の高いRobotoを使用。代替フォントとしてUDゴシックフォントを検討しています。コンセプトは「シェアして続けるヘルスケア」。競合サービスはFiNC、Google FIt等です。",
+  },
+  {
+    "name": "indonesia",
+    "Overview": "The name dates to the 18th century, far predating the formation of independent Indonesia",
+		"History": "Fossilised remains of Homo erectus, popularly known as the 'Java Man', ",
+		"Detail": "Tectonically, Indonesia is highly unstable, making it a site of numerous volcanoes and frequent earthquakes",
+  },
+  {
+    "name": "Japan",
+    "Overview": "The Japanese word for Japan is 日本, which is pronounced Nihon or Nippon and literally means 'the origin of the sun'",
+		"History": "A Paleolithic culture around 30,000 BC constitutes the first known habitation of the Japanese archipelago",
+		"Detail": "Japan has a total of 6,852 islands extending along the Pacific coast. It is over 3,000 km (1,900 mi)",
+  },
+  {
+    "name": "United Kingdom",
+    "Overview": "The 1707 Acts of Union declared that the kingdoms of England and Scotland were 'United into One Kingdom by the Name of Great Britain'.",
+		"History": "Settlement by anatomically modern humans of what was to become the United Kingdom occurred in waves beginning by about 30,000 years ago.",
+		"Detail": "The total area of the United Kingdom is approximately 244,820 square kilometres (94,530 sq mi)",
+  },
+  {
+    "name": "Italia",
+    "Overview": "Hypotheses for the etymology of the name 'Italia' are numerous.",
+		"History": "Thousands of Paleolithic-era artifacts have been recovered from Monte Poggiolo and dated to around 850,000 years before the present",
+		"Detail": "The country's total area is 301,230 square kilometres (116,306 sq mi), of which 294,020 km2 (113,522 sq mi)",
+  },
+
+]
+
